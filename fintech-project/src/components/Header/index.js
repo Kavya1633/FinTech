396fe5874d59ec1192798975c32cb635da9e38ef
Tanisha Jain <tanishajn12@gmail.com>
2024-08-22 12:03:53 +0530

    Day 02

---
+++ fintech-project/src/components/Header/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import "./style.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
+
+
+function Header() {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  function logout() {
+    auth.signOut();
+    navigate("/");
+  }
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    } else {
+      navigate("/dashboard");
+    }
+  }, [user, navigate]);
+
+  return (
+    <div className="navbar">
+      <p className="navbar-heading">SaveMate.</p>
+      {user ? (
+        <p className="navbar-link" onClick={logout}>
+          <span style={{ marginRight: "1rem" }}>
+            {/* <img
+              src={user.photoURL ? user.photoURL : userSvg}
+              width={user.photoURL ? "32" : "24"}
+              style={{ borderRadius: "50%" }}
+            /> */}
+          </span>
+          Logout
+        </p>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
+export default Header;
